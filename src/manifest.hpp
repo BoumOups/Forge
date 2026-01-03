e9f631f214e45fef561f11b617b8b8421b1b8fff
Oups <arthur.kielar@outlook.fr>
2026-01-03 17:26:59 +0100

    Fix the output directory of the manifest.json

--- src/manifest.hpp
+++ src/manifest.hpp
@@ -7,14 +7,14 @@ namespace forge {
     class Manifest {
         public:
         static nlohmann::json load() {
-            std::ifstream file("build/manifest.json");
+            std::ifstream file("bin/manifest.json");
             if (!file.is_open()) return nlohmann::json::object();
 
             return nlohmann::json::parse(file);
         }
 
         static void save(const nlohmann::json& data) {
-            std::ofstream file("build/manifest.json");
+            std::ofstream file("bin/manifest.json");
             file << data.dump();
         }
     };
