81f44b03f4fca1314199d30729c7a815f793b3b4
Oups <arthur.kielar@outlook.fr>
2026-01-07 08:44:12 +0100

    Replace the print statements in executor.hpp by log

--- src/scheduler/executor.hpp
+++ src/scheduler/executor.hpp
@@ -2,29 +2,34 @@
 
 #include <cstdlib>
 #include <future>
+#include <print>
 #include <string>
 #include <vector>
-#include <print>
+
+#include "cli/log.h++"
 
 namespace forge {
-    class Executor {
-        public:
-        static bool execute_parallel(const std::vector<std::string>& commands) {
-            std::vector<std::future<int>> futures;
+class Executor {
+public:
+  static bool execute_parallel(const std::vector<std::string> &commands) {
+    std::vector<std::future<int>> futures;
 
-            for (const auto command : commands) {
-                futures.push_back(std::async(std::launch::async, [command]{
-                    std::print("[Run] {}\n", command);
-                    return std::system(command.c_str());
-                }));
-            }
+    for (const auto command : commands) {
+      futures.push_back(std::async(std::launch::async, [command] {
+        std::print("[Run] {}\n", command);
+        forge::message::log(forge::message::Level::Info,
+                            std::format("Executing command: {}", command));
+        return std::system(command.c_str());
+      }));
+    }
 
-            bool success = true;
-            for (auto& futur : futures) {
-                if (futur.get() != 0) success = false;
-            }
+    bool success = true;
+    for (auto &futur : futures) {
+      if (futur.get() != 0)
+        success = false;
+    }
 
-            return success;
-        }
-    };
-}
+    return success;
+  }
+};
+} // namespace forge
