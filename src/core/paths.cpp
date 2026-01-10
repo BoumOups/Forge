71cf2d4084ac4f95fe71c5e880921c0d8a5ad307
Oups <arthur.kielar@outlook.fr>
2026-01-10 10:37:03 +0100

    Create the get_forge_path function to retrieve the path of forge in the user's disk

--- src/core/paths.cpp
+++ src/core/paths.cpp
@@ -1,5 +1,15 @@
 #include "core/paths.h++"
 
+#include <filesystem>
+#include <string_view>
+
+#if defined(_WIN32)
+#include <windows.h>
+#else
+#include <limits.h>
+#include <unistd.h>
+#endif
+
 namespace forge {
 bool Path::ensure_directory_exists(const std::filesystem::path &path) {
   if (!std::filesystem::exists(path)) {
@@ -20,6 +30,19 @@ const std::string_view Path::get_platform_suffix() {
 #endif
 }
 
+std::filesystem::path forge::Path::get_forge_path() {
+#if defined(_WIN32)
+  wchar_t path[MAX_PATH];
+  GetModuleFileNameW(NULL, path, MAX_PATH);
+  return std::filesystem::path(path).parent_path();
+#else
+  char result[PATH_MAX];
+  ssize_t count = readlink("/proc/self/exe", result, PATH_MAX);
+  return std::filesystem::path(std::string(result, (count > 0) ? count : 0))
+      .parent_path();
+#endif
+}
+
 std::filesystem::path Path::get_output_directory_path() {
   std::filesystem::path output_dir =
       std::filesystem::current_path() / "forge-out";
@@ -34,9 +57,9 @@ std::filesystem::path Path::get_build_script_path() {
 }
 
 std::filesystem::path Path::get_wasm_compiler_path() {
-  std::filesystem::path clang_path = WASI_BASE_PATH.data() +
-                                     std::string(get_platform_suffix()) +
-                                     "/bin/clang++";
-  return clang_path;
+  std::filesystem::path clang_path =
+      std::filesystem::path(WASI_BASE_PATH.data()) /
+      std::string(get_platform_suffix()) / "bin/clang++";
+  return std::filesystem::absolute(clang_path);
 }
 } // namespace forge
--- src/core/paths.h++
+++ src/core/paths.h++
@@ -1,15 +1,17 @@
 #pragma once
 
 #include <filesystem>
+
 namespace forge {
 
 class Path {
 private:
-  static constexpr std::string_view WASI_BASE_PATH = "../vendor/wasi-sdk/";
+  static constexpr std::string_view WASI_BASE_PATH = "vendor/wasi-sdk/";
 
 private:
   static bool ensure_directory_exists(const std::filesystem::path &path);
   static const std::string_view get_platform_suffix();
+  static std::filesystem::path get_forge_path();
 
 public:
   static std::filesystem::path get_output_directory_path();
